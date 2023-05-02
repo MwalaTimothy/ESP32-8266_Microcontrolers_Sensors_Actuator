145af0015a5c081731bd02cf90abc2155aaff973
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-05-02 16:02:07 +0300

    Update BH1750_D1-Mini_0.66-OLED.ino

--- BH1750_D1-Mini_0.66-OLED/BH1750_D1-Mini_0.66-OLED.ino
+++ BH1750_D1-Mini_0.66-OLED/BH1750_D1-Mini_0.66-OLED.ino
@@ -4,39 +4,32 @@
 #include <Adafruit_SSD1306.h>
 #include <BH1750.h>
 
-#define OLED_RESET 0  // GPIO0
+#define OLED_RESET 0 // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
 BH1750 light(0x23);
 
 void setup() {
-
   Serial.begin(115200);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
+  display.clearDisplay();
+  display.setTextColor(WHITE);
 }
 
 void loop() {
-
-
-  // Clear the buffer.
   display.clearDisplay();
-  display.setTextSize(1);
   display.setCursor(2, 0);
-  display.setTextColor(WHITE);
-
+  display.setTextSize(1);
+  
   if (light.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
     display.println("L: ");
-    display.setTextSize(1);
     display.print(light.readLightLevel());
-
   }
-  else
-  {
+  else {
     display.println("Error!");
   }
+  
   display.display();
   delay(1000);
-
 }
