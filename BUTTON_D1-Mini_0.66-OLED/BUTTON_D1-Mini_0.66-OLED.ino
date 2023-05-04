3b033f507842d8fe377cc1dc63096660a67fba81
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-05-04 22:30:18 +0300

    Update BUTTON_D1-Mini_0.66-OLED.ino

--- BUTTON_D1-Mini_0.66-OLED/BUTTON_D1-Mini_0.66-OLED.ino
+++ BUTTON_D1-Mini_0.66-OLED/BUTTON_D1-Mini_0.66-OLED.ino
@@ -1,17 +1,4 @@
-/*
- * 1 Button Shield - Switch Library
- * Examples using Albert van Dalen's Switch Library
- * http://www.avdweb.nl/arduino/hardware-interfacing/simple-switch-debouncer.html
- *
- * External pull-up resistors are not required.
- * Supports also long press and double clicks.
- * Performs not just de-bouncing, but also de-glitching against EMC pulses.
- *
- * Licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
- *
- * 1 Button Shield pushbutton connects pin D3 to GND
- */
- #include <Wire.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "avdweb_Switch.h"
@@ -19,61 +6,62 @@
 #define OLED_RESET 0 // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
-const byte buttonPin = D3;
+const byte BUTTON_PIN = D3;
 
-int i;
+Switch button = Switch(BUTTON_PIN);  // Switch between a digital pin and GND
 
-Switch button = Switch(buttonPin);  // Switch between a digital pin and GND
+int clickCount = 0;
 
 void setup() {
   Serial.begin(9600);
-  display.println("Examples using Switch library");
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("Examples using Switch library");
+  display.display();
 }
 
 void loop() {
-      
   button.poll();
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setCursor(45, 15);
-  display.setTextColor(WHITE);
- 
+
   if (button.switched()) {
-     display.setTextSize(1);
-    display.setCursor(45, 13);
-    display.print("switched");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Switched");
     display.display();
   }
 
   if (button.pushed()) {
-    display.setTextSize(1);
-    display.setCursor(45, 21);
-    display.print("pushed ");
-    display.print(++i);
-    display.print(" ");
+    clickCount++;
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Pushed");
+    display.setCursor(0, 10);
+    display.print("Click Count: ");
+    display.print(clickCount);
     display.display();
   }
 
   if (button.longPress()) {
-     display.setTextSize(1);
-    display.setCursor(45, 15);
-    display.print("longPress ");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Long Press");
     display.display();
   }
 
   if (button.doubleClick()) {
-    display.setTextSize(1);
-    display.setCursor(45, 10);
-    display.print("doubleClick ");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Double Click");
     display.display();
   }
 
   if (button.released()) {
-    display.setTextSize(1);
-    display.setCursor(45, 21);
-    display.print("released");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Released");
     display.display();
   }
-    
 }
