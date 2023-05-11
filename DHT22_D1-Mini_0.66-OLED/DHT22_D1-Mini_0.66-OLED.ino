62de8d14421821c59327817f46c59db83a2bb403
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2023-05-11 20:09:02 +0300

    Update DHT22_D1-Mini_0.66-OLED.ino

--- DHT22_D1-Mini_0.66-OLED/DHT22_D1-Mini_0.66-OLED.ino
+++ DHT22_D1-Mini_0.66-OLED/DHT22_D1-Mini_0.66-OLED.ino
@@ -3,49 +3,66 @@
 #include <Adafruit_GFX.h>
 #include "Adafruit_SSD1306.h"
 #include <DHT.h>
- 
-#define OLED_RESET 0  // GPIO0
+
+#define OLED_RESET 0 // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
- 
+
 #define DHTPIN D4
-#define DHTTYPE DHT22  
+#define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
- 
-void setup()   
+
+void setup()
 {
   Serial.begin(115200);
   dht.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
- display.display();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C (for the 64x48)
+  display.display();
 }
- 
-void loop() 
+
+void loop()
 {
   delay(1000);
 
-
+  // Read sensor values
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   float f = dht.readTemperature(true);
-  float bat = ESP.getVcc();// / 1024;
-  // Check if any reads failed and exit early (to try again). 
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  float bat = ESP.getVcc();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
     display.println("Failed to read from DHT sensor!");
     return;
   }
-         // Efface l'écran et positionne le curseur dans le coin supérieur gauche - clear display and set cursor on the top left corner
+
+  // Clear the display and set the cursor to the top-left corner
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  // Température en Celcius - Temperature in Celcius
-  display.println("Temp.");
+  
+
+  // clear buffer and create a 1st page to Display temperature in Celsius
+  display.clearDisplay();
+  display.setCursor(32,10);
+  display.println("Temperature.");
+  display.setCursor(32,19);
   display.print(t);
-  display.println(" c");
-  // Humidité en % - Humidity in % 
+  display.println(" C");
+
+  display.display();
+  delay(3000);
+  
+
+  // clear buffer and create a 2nd page to Display humidity
+  display.clearDisplay();
+  display.setCursor(32,10);
   display.println("Humidity");
+  display.setCursor(32,19);
   display.print(h);
   display.println(" %");
+
   display.display();
+
+  delay(3000);
 }
