511875d23590badd639ab9916a1bca396ba52de4
Timothy Mwala <74060530+MwalaTimothy@users.noreply.github.com>
2024-08-21 17:33:49 +0300

    Update PIR_C3-MINI_0.66_Display.ino

--- PIR_C3-MINI_0.66-OLED/PIR_C3-MINI_0.66_Display.ino
+++ PIR_C3-MINI_0.66-OLED/PIR_C3-MINI_0.66_Display.ino
@@ -2,45 +2,46 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define OLED_RESET 7 //Correspond to GPIO0 on ESP8266
+#define OLED_RESET -1 // Use -1 if there's no reset pin for the OLED
 Adafruit_SSD1306 display(OLED_RESET);
 
-const int PIR = 7;
-int PIRState = 7;
- 
+const int PIR = 7; // Use a different pin for PIR sensor
+int PIRState = 0;
+
 void setup() 
 {
   Serial.begin(9600);
-    Wire.begin(8,10);
+  // Initialize display with I2C address 0x3C
+  Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE);
- delay(2000);
- 
+  display.clearDisplay();
+  display.display();
+
+  // Initialize PIR sensor pin
   pinMode(PIR, INPUT);
- 
-   
 }
- 
+
 void loop() 
 {
   PIRState = digitalRead(PIR);
- 
+
   if (PIRState == HIGH) 
   {
     display.clearDisplay();
     display.setTextSize(1);
-    display.setCursor(35, 15);
-    display.write("INTRUDER!\n");
+    display.setCursor(25, 15);
+    display.print("INTRUDER!");
     display.display();
-    
   } 
   else 
   {
     display.clearDisplay();
     display.setTextSize(1);
-    display.setCursor(40,15);
-    display.write("No motion\n");
+    display.setCursor(30, 15);
+    display.print("No Motion");
     display.display();
   }
+  
   delay(1000);
 }
